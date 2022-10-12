6a415f2b662b2bb09717d9a8f6c648616c6e7cb7
Ben Shelton <bks-44@hotmail.com>
2022-10-12 16:19:26 -0500

    initial commit

--- Tabloid/client/src/components/Category/CategoryList.js
+++ Tabloid/client/src/components/Category/CategoryList.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getAllCategories } from "../../modules/categoryManager";
 import { Category } from "./Category";
 import "./Category.css";
+import { Form, FormGroup } from "reactstrap"
+import { addCategory } from "../../modules/categoryManager"
+import { CreateCategory } from "./CreateCategory";
 
 export const CategoryList = () => {
     const [categories, setCategories] = useState([])
+    const [category, update] = useState({
+        Name: ""
+    })
 
     const getCategories = () => {
         getAllCategories().then(categories => setCategories(categories))
@@ -15,6 +22,25 @@ export const CategoryList = () => {
     },
         []
     )
+    const handleReset = () => {
+        Array.from(document.querySelectorAll("input")).forEach(
+            input => (input.value = "")
+        );
+        // this.setState({
+        //     itemvalues: [{}]
+        // });
+    };
+
+    const handleCreateButtonClick = (event) => {
+        event.preventDefault()
+        const categoryToSendToApi = {
+            Name: category.Name
+        }
+
+        addCategory(categoryToSendToApi)
+        getCategories()
+        handleReset()
+    }
 
     return (
         <div className="container">
@@ -30,6 +56,28 @@ export const CategoryList = () => {
                         <Category category={category} key={category.id} />
                     ))
                 }
+                <Form id="addCategoryForm">
+                    <FormGroup>
+                        <fieldset>
+                            <div className="formInputsContainer">
+                                {/* <label htmlFor="name">Category Name:</label> */}
+                                <input type="name"
+                                    className="createCategoryInput"
+                                    placeholder="Create a new category.."
+                                    onChange={
+                                        (evt) => {
+                                            let copy = { ...category }
+                                            copy.Name = evt.target.value
+                                            update(copy)
+                                        }
+                                    } />
+                                <button onClick={(clickEvent) => { handleCreateButtonClick(clickEvent) }}
+                                    className="categorySaveButton" >SAVE</button>
+                            </div>
+                        </fieldset>
+                    </FormGroup>
+                </Form>
+                {/* <Link to={"/category/create"}>CREATE CATEGORY</Link> */}
             </div>
         </div>
     )
