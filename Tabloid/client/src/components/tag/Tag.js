289a2d7a11df0b8cae5c5065d5b2a21e7af051bb
Stephen Smith <StephenASmith7178@gmail.com>
2022-10-12 11:36:41 -0400

    styles in tag

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -1,39 +1,42 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { Button, Card, CardBody } from "reactstrap";
+import { Card, CardBody } from "reactstrap";
 import { getAllTags } from "../../modules/tagManager";
 import { deleteTag } from "../../modules/tagManager";
 
+
+
 const Tag = ({ tag }) => {
-
-
-  //func to delete workout from database if has wworkoutid
+  //func to delete workout from database if has workoutid
   const deleteButton = (tagId) => {
-   deleteTag(tagId)
-   getAllTags()
-  }
 
+    deleteTag(tagId)
+    getAllTags()
+  }
 
   return (
     <Card>
-      <CardBody>
-        <p>
-          <strong>{tag.name}</strong>
-        </p>
+      <CardBody id="tag-cardBody">
+        <section className="tagContainer">
+          <div className="tagNameContainer">
+            <span className="tagName">{tag.name}</span>
+          </div>
+          <div className="buttonContainer">
+            <button outline onClick={() =>
+              Navigate(`/tag/edit/${tag.id}`)}
+              className="editButton" >
+              EDIT
+            </button>
+            <button outline onClick={() => deleteButton(tag.id)}
+              className="deleteButton">
+              DELETE
+            </button>
+          </div>
+        </section>
       </CardBody>
-      
-      <Button outline onClick={() =>
-                Navigate(`/tag/edit/${tag.id}`)}
-                className="pro-primary" >
-                Edit
-              </Button>
-      <Button outline onClick={() => deleteButton(tag.id)}
-                className="pro-primary">
-                Delete 
-              </Button>
-    
     </Card>
-  );
+
+);
 };
 
 export default Tag;
--- Tabloid/client/src/components/tag/TagList.js
+++ Tabloid/client/src/components/tag/TagList.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Tag from './Tag';
 import { getAllTags } from "../../modules/tagManager";
-import { Button, NavItem, NavLink } from "reactstrap";
-import { NavLink as RRNavLink } from "react-router-dom";
+import { Button, NavItem } from "reactstrap";
+import { Navigate, NavLink as RRNavLink } from "react-router-dom";
+import "./Tag.css";
 
 export default function TagList() {
   const [tags, setTags] = useState([]);
@@ -16,21 +17,26 @@ export default function TagList() {
   }, [tags]);
 
   return (
-    <>
-        <p className="text-left px-4"><strong>Tag Options:</strong></p>
-        <NavItem>
-          <Button tag={RRNavLink} to="/tag/create">Create Tag</Button>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="logoContainer">
+          <span className="logoCircle">
+            <img className="quillLogo" src={process.env.PUBLIC_URL + "/quill-logo.png"} />
+          </span>
+        </div>
+        <h1 className="tagPageHeader">TAG MANAGEMENT</h1>
+        {
+          tags.map((tag) => (
+            <Tag tag={tag} key={tag.id} />
+          ))
+        }
+        <NavItem className="addTagContainer">
+          <div className="addTagContainer">Add a new tag</div>
+          <Button id="createButton" tag={RRNavLink} to="/tag/create">Create</Button>
         </NavItem>
-        
-      <section>
-        {tags.map((tag) => (
-          <Tag key={tag.id} tag={tag} />
-        ))}
-      </section>
-    </>
-  );
+
+      </div>
+    </div>
+  )
 }
-
-
-
 
